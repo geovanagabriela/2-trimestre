51ebf6299e7f288f3670d8ebef9d5989bec22856
geovanagabriela <148064018+geovanagabriela@users.noreply.github.com>
2024-05-28 10:11:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
+const caixaPrincipal=document.queryselector(".caixa-principal");
+const caixaPerguntas=document.queryselector('.caixa-perguntas");
+const caixaAlternativas=document.querysector(".caixa-alternativas");
+const caixaResultado=document.queryselector(".caixa-resultado");
+const textoResultado=document.queryselector(".texto-resultado");
 
+const perguntas=[
+  {
+    enunciado:"voce prefere inverno ou verão?",
+    alternativas:[
+      "inverno?",
+      "verão?",
+      ]
+  }
+   {
+    enunciado:"voce prefere praia ou piscina?",
+    alternativas:[
+      "praia?",
+      "piscina?",
+      ]
+  }
+     {
+    enunciado:"voce prefere cachorro ou gato?",
+    alternativas:[
+      "cachorro",
+      "gato",
+      ]
+  }                                        
+ {
+    enunciado:"voce prefere sorvete ou açai?",
+    alternativas:[
+      "sorvete",
+      "açai",
+      ]
+  }
+ {
+    enunciado:"voce prefere lasanha ou pizza?",
+    alternativas:[
+      "lasanha",
+      "pizza",
+      ]
+  }
+];
